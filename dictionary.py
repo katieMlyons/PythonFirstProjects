613c50731ebf6962b19aa5ab139667376f4eb4ae
KMLyons <klyons19@gmail.com>
2020-12-09 22:15:19 -0600

    cleaning up

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,7 @@
 # load in .json file, take input and return value
 import json
 from difflib import get_close_matches
-search_string=input("Enter word:")
+search_string=input("Enter word: ")
 
 data=json.load(open("data.json"))
 
@@ -9,19 +9,24 @@ def dict_out(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif len(get_close_matches(w,data.keys(),1))>0:
-        answer = input("Did you mean {}? Y or N:".format(get_close_matches(w,data.keys(),1)[0]))
-        if answer.lower() == "y":
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
+    elif len(get_close_matches(w,data.keys())) > 0:
+        yn = input("Did you mean %s? Y or N: " % get_close_matches(w,data.keys())[0])
+        if yn == "Y":
             return data[get_close_matches(w,data.keys(),1)[0]]
-        elif answer.lower() == "n":
+        elif yn == "N":
             return "Word not found."
         else:
             return "I don't understand."
     else:
         return "Word not found."
 
-if type(dict_out(search_string)) == list:
-    for i in dict_out(search_string):
+output = dict_out(search_string)
+if type(output) == list:
+    for i in output:
         print(i)
 else: 
-    print(dict_out(search_string))
+    print(output)
