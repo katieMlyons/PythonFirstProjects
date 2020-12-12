e250477176f94a6f3f328fc522ea621d4ea72909
KMLyons <klyons19@gmail.com>
2020-12-11 18:24:00 -0600

    finished webmap

--- mapping/webmap.py
+++ mapping/webmap.py
@@ -6,8 +6,14 @@ lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
 
-def color_producer():
-    return "green"
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red" 
+    
 
 html = """<h4>Volcano information:</h4>
 Height: %s m
@@ -15,13 +21,21 @@ Height: %s m
 
 map = folium.Map(location=[38.58,-99.09],zoom_start=6, tiles = 'Stamen Terrain')
 
-fg = folium.FeatureGroup(name = "My Map")
+fgv = folium.FeatureGroup(name = "Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt,ln], popup=html % str(el), icon=folium.Icon(color=color_producer())))
+    fgv.add_child(folium.CircleMarker(location=[lt,ln], popup=html % str(el), 
+    fill_color=color_producer(el), color="grey", radius=6, fill_opacity=0.7, fill=True))
 
+style_function=lambda x: {"fillColor":"green" if x['properties']['POP2005'] < 10000000 
+                            else "orange" if 10000000 <= x['properties']['POP2005'] < 20000000 else "red"}
+fgp = folium.FeatureGroup(name = "Population")
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),style_function=style_function))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
 
 map.save("Map1.html")
 
