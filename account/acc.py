5fa095b4042a670a4a6148e6f74be1db590bf765
KMLyons <klyons19@gmail.com>
2020-12-16 19:59:39 -0600

    finished acc object

--- account/acc.py
+++ account/acc.py
@@ -15,9 +15,23 @@ class Account:
         with open(self.filepath, "w") as file:
             file.write(str(self.balance))
 
-account = Account("balance.txt")
-print(account.balance)
-#account.withdraw(100)
-account.deposit(200)
-print(account.balance)
-account.commit()
+class Checking(Account):
+
+    type = "checking"
+
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee = fee
+
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+
+jacks_checking = Checking("Jack.txt", 1)
+jacks_checking.transfer(100)
+jacks_checking.commit()
+print(jacks_checking.balance)
+
+johns_checking = Checking("John.txt", 1)
+johns_checking.transfer(100)
+johns_checking.commit()
+print(johns_checking.balance)
