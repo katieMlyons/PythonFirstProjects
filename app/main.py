c7babc1f95784d46392329729f78d6fa969b013e
KMLyons <klyons19@gmail.com>
2020-12-19 19:28:03 -0600

    fixed success screen

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ import json, glob
 from datetime import datetime
 from pathlib import Path
 import random
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.animation import Animation
+from kivy.uix.behaviors import ButtonBehavior
 
 Builder.load_file("design.kv")
 
@@ -53,6 +57,9 @@ class LoginScreenSuccess(Screen):
             self.ids.quote.text = random.choice(quotes)
         else:
             self.ids.quote.text = "Try another feeling"
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
 
 class MainApp(App):
     def build(self):
