b2e038a31f39f9590acd93ea974e14a4f296a9f2
KMLyons <klyons19@gmail.com>
2020-12-22 18:11:42 -0600

    init capture

---
+++ capture.py
@@ -0,0 +1,26 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+
+a = 0
+
+while True:
+    a+=1
+    check, frame = video.read()
+
+    print(check)
+    print(frame)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Capture', gray)
+
+    key = cv2.waitKey(1)
+    print(gray)
+    
+    if key == ord('q'):
+        break
+
+print(a)
+video.release()
+cv2.destroyAllWindows()
